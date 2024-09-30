d1a124940adfef80b3b43e32e989f60ded27df89
EllieYT <ellie.tangs@gmail.com>
2024-09-29 21:14:53 -0700

    w4d2 commit 1

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -3,8 +3,6 @@
 // Function to generate combination of characters
 function generateCode() {
   // Create variables to store generated codes and the type of characters we want to show as codes
-  var code = " "; // initialize to null value
-  var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
 
   // Generate character multiple times using a loop
   for (i = 1; i <= 8; i++) {
@@ -24,3 +22,23 @@ function disableButton() {
 
 // Activate function
 disableButton();
+
+//NOTE: submit button is initially disabled upon loading of this page - see <body> in html
+var code = " "; // to store generated codes and initialize to empty value
+var getCode = " "; //to store entered code
+var btnvalue; //for button boolean value
+
+//determne when to able or disable button
+function disableButton(btnvalue) {
+  document.getElementById("submit").disabled = btnvalue; // able/disable button
+  if (btnvalue == true) {
+    // test if button is disabled or enabled
+    // set button and label color translucent
+    document.getElementById("submit").style.backgroundColor =
+      "rgba(73, 119, 209, 0.3)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+  }
+}
+
+//create variable to hold the type of characters we want to show as codes
+var str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$";
